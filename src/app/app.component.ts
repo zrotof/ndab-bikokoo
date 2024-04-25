dfaf94ecfa37e34bc28b86c3389362a71dd1c98e
zrotof <manduel21@gmail.com>
2024-04-26 00:26:31 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFaceLaugh } from '@fortawesome/free-regular-svg-icons'
+import { faFaceMehBlank, faFaceMeh, faFaceSmile } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'purs-font-admin';
+
+  title ="";
+  
+  faFaceSmile = faFaceSmile;
+  faFaceMehBlank = faFaceMehBlank;
+  faFaceMeh = faFaceMeh;
+  faFaceLaugh = faFaceLaugh;
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
